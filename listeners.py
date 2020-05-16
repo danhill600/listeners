21dc3b6dfd3ee66448547f954b5c0c31c877200d
Dan Hill <dhill@witsquash.com>
2020-05-16 10:58:37 -0500

    switched Popen to run in for loop

--- listeners.py
+++ listeners.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import subprocess
 from subprocess import Popen, PIPE
 
 listeners = []
@@ -8,9 +9,9 @@ pipe = Popen('listeners.sh', shell=True, stdout=PIPE)
 for i in pipe.stdout:
     listeners.append(i.strip().decode())
 
+print(listeners)
 counter = 1
 for i in listeners:
     print('Listener ' + str(counter) + ':')
-    Popen(['ipgeo.sh',i])
-    print("\n")
+    subprocess.run(['ipgeo.sh',i])
     counter += 1
